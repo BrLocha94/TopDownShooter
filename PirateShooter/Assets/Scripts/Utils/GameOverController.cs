530d4bb40255be7de5488eaae98a4476dbd966eb
bruno.locha <bruno.locha@hydrasystems.com.br>
2022-10-09 20:13:18 -0300

    Created Game clear scene

---
+++ PirateShooter/Assets/Scripts/Utils/GameOverController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverController : MonoBehaviour
+{
+    [Header("External references")]
+    [SerializeField]
+    private Text titleText;
+    [SerializeField]
+    private Text messageText;
+
+    private void Awake()
+    {
+        bool gameClear = StateMachineController.gameState == GameState.GameClear;
+
+        titleText.text = gameClear ? "CONGRATULATIONS!!!" : "GAME OVER";
+
+        string message = string.Empty;
+
+        if (gameClear)
+            message = "You survived the battle and sank " + PersistentData.shipsDestroyed + " ships!";
+        else
+            message = "You fought bravely, but didn't survived...";
+
+        messageText.text = message;
+    }
+}
